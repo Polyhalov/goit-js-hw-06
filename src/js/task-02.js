0598ce7a6299aca7e1240daa8c57aa2224aac179
Polyhalov <apolyhalov@gmail.com>
2022-12-01 11:23:37 +0200

    2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulList = document.querySelector('#ingredients');
+const elements = ingredients.map(ingredient => {
+  const newLi = document.createElement('li');
+  newLi.classList.add('item');
+  newLi.textContent = `${ingredient}`;
+  return newLi
+});
+console.log(elements)
+
+ulList.append(...elements)
+console.log(ulList)
