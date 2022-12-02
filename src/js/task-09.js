144d15e1b7b90c235164f08328bb4bb6809c241f
Polyhalov <apolyhalov@gmail.com>
2022-12-02 19:01:39 +0200

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const text = document.querySelector('.color');
+
+changeBtn.addEventListener('click', changeColor)
+function changeColor(event) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  text.textContent = color; 
+}
